146c438e9448ae6696b03ba77d797f90769c8036
SILVANA-ON <0000108823477xsp@al.educacao.sp.gov.br>
2024-08-28 08:17:09 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+    createCanvas(800, 800);
+   background("pink");
+  }
+  
+  function draw() {
+   //console.log(mouseIsPressed)
+    
+    
+    fill("purple")
+    stroke(0)
+    
+      if(mouseIsPressed){ 
+    
+    rect(mouseX,mouseY,30,35);
+    
+  
+  }
+  }
